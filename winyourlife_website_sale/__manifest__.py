b703993ff3d843e7b7accdfd322632eca1ffad2c
Joachim Roekens <j.roekens@deuse.be>
2022-07-08 14:46:44 +0200

    Phone and email on shop confirm

--- winyourlife_website_sale/__manifest__.py
+++ winyourlife_website_sale/__manifest__.py
@@ -6,7 +6,7 @@
     'sequence': 50,
     'summary': 'Winimo customizations of eCommerce',
     'website': 'deuse@deuse.be',
-    'version': '15.0.1.0.1',
+    'version': '15.0.1.0.2',
     'description': "",
     'depends': ['website_sale'],
     'data': [
@@ -14,7 +14,7 @@
         'templates/address.xml',
         'templates/login.xml',
         'templates/checkout.xml',
-        # 'templates/payment.xml',
+        'templates/payment.xml',
     ],
     'images': [
         'static/description/icon.png',
